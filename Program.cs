e00224754c280bfde2ad2b0fb2a5774f078192c8
arminegilavyan04 <71060697+arminegilavyan04@users.noreply.github.com>
2024-10-22 16:08:29 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "racecar";
+        bool result = Polindrome(input);
+        Console.WriteLine(result); 
+    }
+    static bool Polindrome(string str)
+    {
+        str = str.ToLower();
+        int left = 0;
+        int right = str.Length - 1;
+
+        while (left < right)
+        {
+            if (str[left] != str[right])
+            {
+                return false; 
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
